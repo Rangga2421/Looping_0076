c6d93c78df7d1fbe3b4b1e4918d2df9b651349bf
Rangga2421 <ranggaardisaputra123@gmail.com>
2026-03-31 20:30:16 +0700

    Menambahkan prosedur input

--- switchCase.cpp
+++ switchCase.cpp
@@ -6,6 +6,17 @@ int kodeJurusan;
 string namaJurusan;
 string statusKuota;
 
+//prosedur input
+void input(){
+    cout <<"=== PENDAFTARAN JURUSAN KAMPUS ===" << endl;
+    cout << "1. TEKNOLOGI INFOMASI" << endl;
+    cout << "2. TEKNIK ELEKTRO" << endl;
+    cout << "3. SISTEM INFORMASI" << endl;
+    cout << "--------------------------------" << endl;
+    cout << "Masukkan kode jurusan (1-3) : ";
+    cin >> kodeJurusan;
+}
+
 int main(){
 
     return 0;
